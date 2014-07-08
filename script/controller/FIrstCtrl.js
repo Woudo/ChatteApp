f8a6e750fdd60a0889a25ba4dce2c1d19deeb6a0
Woudo <loudo31@hotmail.fr>
2014-07-08 21:13:34 +0200

    yo

---
+++ script/controller/FIrstCtrl.js
@@ -0,0 +1,10 @@
+function FirstCtrl ($scope, $firebase){
+    var ref = new Firebase ('https://chatteapp.firebaseio.com/');
+    $scope.messages = $firebase(ref);
+    
+    $scope.addMeassage = function(e){
+        if (e.KeyCode != 13) return,
+            $scope.messages.$add({from: $scope.name, body: $scope.msg});
+            $scope.messages = '';
+        };
+    }

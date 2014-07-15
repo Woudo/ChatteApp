18da0316dff749602869c05839e0da00b2e8cf8d
Woudo <loudo31@hotmail.fr>
2014-07-15 18:41:47 +0200

    library fix

--- script/controller/FIrstCtrl.js
+++ script/controller/FIrstCtrl.js
@@ -2,7 +2,7 @@ function FirstCtrl ($scope, $firebase){
     var ref = new Firebase ('https://chatteapp.firebaseio.com/');
     $scope.messages = $firebase(ref);
     
-    $scope.addMeassage = function(e){
+    $scope.addMessage = function(e){
         if (e.KeyCode != 13) return,
             $scope.messages.$add({from: $scope.name, body: $scope.msg});
             $scope.messages = '';
